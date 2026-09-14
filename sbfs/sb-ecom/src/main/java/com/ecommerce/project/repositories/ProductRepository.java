b5ccdb8e9fec53ca5d8f25b532183f00f9ef3f02
unknown <jo@towa300.towasystem.co.jp>
2026-09-14 12:02:55 +0900

    finish p155

--- sbfs/sb-ecom/src/main/java/com/ecommerce/project/repositories/ProductRepository.java
+++ sbfs/sb-ecom/src/main/java/com/ecommerce/project/repositories/ProductRepository.java
@@ -1,9 +1,16 @@
 package com.ecommerce.project.repositories;
 
+import com.ecommerce.project.model.Category;
 import com.ecommerce.project.model.Product;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    // methodを定義するだけです。実装はSpring Data JPAが自動で行います。
+    List<Product> findByCategoryOrderByPriceAsc(Category category);
 }
--- sbfs/sb-ecom/src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
+++ sbfs/sb-ecom/src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
@@ -1,7 +1,60 @@
 package com.ecommerce.project.service;
 
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.ecommerce.project.exceptions.ResourceNotFoundException;
+import com.ecommerce.project.model.Product;
+import com.ecommerce.project.model.Category;
+import com.ecommerce.project.payload.ProductDTO;
+import com.ecommerce.project.payload.ProductResponse;
+import com.ecommerce.project.repositories.CategoryRepository;
+import com.ecommerce.project.repositories.ProductRepository;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+
+    @Autowired 
+    private ProductRepository productRepository;
+
+    @Autowired 
+    private CategoryRepository categoryRepository;
+
+    @Autowired 
+    private ModelMapper modelMapper;
+    
+    @Override
+    public ProductDTO addProduct(Long categoryId, Product product) {
+        Category category = categoryRepository.findById(categoryId)
+            .orElseThrow(() ->
+                new ResourceNotFoundException("Category", "CategoryId", categoryId));
+        product.setImage("default.png");
+        double specialPrice = (100 - product.getDiscount()) * product.getPrice() / 100;
+        product.setSpecialPrice(specialPrice);
+        Product savedProduct = productRepository.save(product);
+        return modelMapper.map(savedProduct, ProductDTO.class);
+    }
+
+    @Override
+    public ProductResponse getAllProducts() {
+        List<Product> products = productRepository.findAll();
+        List<ProductDTO> productDTOs = products.stream()
+            .map(product -> modelMapper.map(product, ProductDTO.class))
+            .toList();
+
+        ProductResponse productResponse = new ProductResponse();
+        productResponse.setContent(productDTOs);
+        return productResponse;
+    }
+
+    @Override
+    public ProductResponse searchByCategory(Long categoryId) {
+        Category category = categoryRepository.findById(categoryId)
+            .orElseThrow(() ->
+                new ResourceNotFoundException("Category", "CategoryId", categoryId));
+        List<Product> products = productRepository.findByCategoryOrderByPriceAsc(category);
+    }
 }
